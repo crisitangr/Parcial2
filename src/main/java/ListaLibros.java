3ca362eb32be21811d9bcbb73d5692bccb0e81f8
Cristian Gracia <est.cristian.gracia@unimilitar.edu.co>
2026-04-16 10:15:22 -0500

    Se respondio la primera pregunta

--- src/main/java/ListaLibros.java
+++ src/main/java/ListaLibros.java
@@ -6,24 +6,30 @@ public class ListaLibros extends ListaEnlazada<Libro> {
 
     // Constructor
     public ListaLibros() {
-
         // Llama al constructor de la clase padre
         super(); // inicializa head y size
     }
-
     // Método equivalente a "agregar" 
     public void agregar(Libro l) {
-
-        
         // Este método ya se encarga de crear nodos y enlazarlos
         this.add(l);
     }
-
     // Método para obtener el primero 
     public Libro getPrimero() {
 
         // Retorna el primer elemento de la lista
         return this.getFirst();
     }
+        public Libro buscarPorTitulo(String titulo) {
 
+        for (int i = 0; i < this.size(); i++) {
+            Libro l = this.get(i);
+
+            if (l.getTitulo().equalsIgnoreCase(titulo)) {
+                return l;
+            }
+        }
+
+        return null;
+    }
 }
